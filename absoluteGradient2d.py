2b500166a7388a511be06b039f0ba91bac3ce793
Eray Inanc <eray.inanc@uni-due.de>
2021-01-14 17:23:39 +0100

    Create absoluteGradient2d.py

---
+++ absoluteGradient2d.py
@@ -0,0 +1,39 @@
+# efficient absolute gradient of 2D field by Eray Inanc (eray.inanc@uni-due.de)
+# note: Compute 2D gradient once, and uses that to compute \nabla \dot Field
+# libs: import numpy as np
+# usage: absoluteGradient = Grad2D(Field(n^2), delta=<float>), sets delta to 1 m if not given
+# return is a 2D field with absolute gradient
+# delta is cell size in meters
+
+# gradient of 2D field
+def Grad2D(Field,delta):
+    # parameters
+    Ima,Kma = Field.shape[:]
+
+    # allocate
+    Grad = np.zeros((Ima+2,Kma+2,2))
+
+    # compute 
+    Grad[1:Ima-1,1:Kma-1,0] = (0.5 / delta) * \
+        ((Field[2:Ima,1:Kma-1] - Field[0:Ima-2,1:Kma-1]))
+    Grad[1:Ima-1,1:Kma-1,1] = (0.5 / delta) * \
+        ((Field[1:Ima-1,2:Kma] - Field[1:Ima-1,0:Kma-2]))
+    
+    return Grad 
+
+# abs gradient of a 2D field
+def AGrad2D(Field,**kwargs):
+    # parameters
+    delta = kwargs.get('delta', None)
+    if not isinstance(delta, float):
+        delta = 1.0
+        print('delta is set to 1 m')
+        
+    # compute 
+    GradF = Grad2D(Field,delta)
+    ABSGrad = np.sqrt(GradF[:,:,0]**2.0 + GradF[:,:,1]**2.0)
+    return ABSGrad 
+
+
+
+#EOF
